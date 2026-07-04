d23e866a64d6d22b58095764ac5df02aa9df8771
Arvi <artemtolstykn@gmail.com>
2026-07-04 10:54:20 +0300

    init avalonia project

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PixelPainter_Avalonia;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
