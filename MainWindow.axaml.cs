169a48d026321fe07e1839f77bb10195b6e7c020
Arvi <artemtolstykn@gmail.com>
2026-07-04 11:47:26 +0300

    add file picker logic

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 
 namespace PixelPainter_Avalonia;
 
@@ -7,5 +8,28 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private async void OnChooseFile(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Choose image",
+            AllowMultiple = false,
+            FileTypeFilter = new[]
+            {
+                new FilePickerFileType("Images")
+                {
+                    Patterns = new[] { "*.png", "*.jpg", "*.jpeg"}
+                }
+            }
+        });
+        if (files.Count >= 1)
+        {
+            var fullPath = files[0].Path.LocalPath;
+            FilePathLabel.Content = fullPath;
+        }
     }
 }
